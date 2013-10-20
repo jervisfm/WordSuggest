8319fa86ad1932cc6f7d201c06846535d3f058e8
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-10-20 09:04:05 +0000

    Add word suggest module

---
+++ word_suggest.py
@@ -0,0 +1,12 @@
+
+
+__author__ = 'Jervis Muindi'
+__date__ ='October 2013'
+
+
+class WordSuggest(object):
+    """Analyzes input textual data to make recommendations for a word given current
+    word. 
+    """
+
+    
