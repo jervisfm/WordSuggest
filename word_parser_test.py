ed7e5e6d96056d83547fc1cbc5cffe775b952884
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-10-20 08:19:59 +0000

    Add test for word parser

--- word_parser_test.py
+++ word_parser_test.py
@@ -5,11 +5,16 @@ __date__ = 'October 2013'
 import unittest
 
 from word_parser import TextStream
+from word_parser import WordParser
+
+
+TEXT_INPUT = "test_input.txt"
 
 class TextStreamTest(unittest.TestCase):
 
     def setUp(self):
-        self.sample_input_file = "test_input.txt"
+        global TEXT_INPUT
+        self.sample_input_file = TEXT_INPUT
 
     
     def test_get_character(self):
@@ -31,6 +36,50 @@ class TextStreamTest(unittest.TestCase):
                 break
         self.assertEqual(actual_content, expected_content, 
                          msg="Expected: %s \n Vs Actual: %s" % (expected_content,actual_content))
+
+
+class WordParserTest(unittest.TestCase):
+
+    def setUp(self):
+        global TEXT_INPUT
+        self.sample_input_file = TEXT_INPUT
+    
+    def get_words_from_file(self, file_path):
+        contents = ""
+        with open(file_path, "r") as f:
+            contents = f.read()
+
+        words = contents.split()
+        
+        # Drop  last "." if present
+        result = []
+        for word in words:
+            if word[-1] == ".":
+                word = word[:-1]
+                result.append(word)
+            else:
+                result.append(word)
+                
+        return result
+
+    def test_get_word(self):
+        
+        # Get all the words in text input file.
+        file_path = self.sample_input_file
+        expected_words = self.get_words_from_file(file_path)
+        
+        
+        # Now try to use WordParser to extract words
+
+        text_stream = TextStream(file_path)
+        parser = WordParser(text_stream)
+        actual_words = []
+        while True:
+            word = parser.get_word()
+            if word:
+                actual_words.append(word)
+            else:
+                break
 
 
 if __name__ == '__main__':
