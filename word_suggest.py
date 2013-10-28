847441ba3fd62ad4b5804d0627006b63e7615264
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-10-28 02:06:30 +0000

    Add a first pass implementation of the WordSuggestBuilder

--- word_suggest.py
+++ word_suggest.py
@@ -6,6 +6,7 @@ __date__ ='October 2013'
 
 from word_parser import TextStream
 from word_parser import WordParser
+from word_parser import WordCount
 
 class WordSuggestBuilder(object):
     """Analyzes input textual data to build a datastructure that will be useful to make
@@ -32,7 +33,44 @@ class WordSuggestBuilder(object):
 
     def process_input(self):
         """Process the text stream input to generate a 
-        dictionary of: word ->  { word -> word_count } for each
+        dictionary of: current_word ->  { next_word -> WordCount Object } for each
         word pair in the text stream."""
-        pass
+
+        parser = self.parser
+        result = {}
+        # TODO(jervis): Current processing assumes that the output
+        # will fit in memory. For really large datasets, this assumption
+        # won't be true; will need to work with that. In that scenario, 
+        # it would be simplest to actually just write all word pairs to disk
+        # and sort them using external merge sort. This will be slow due to 
+        # the sorting; may be worthwhile to consider alternative faster approaches. 
+        while True:
+            curr, next = parser.get_word_pair()
+
+            if not next:
+                # We're done
+                break
         
+            if curr in result:
+                # Retrieve and use the existing value dict
+                value_dict = result[curr]
+                
+                # Check if this subseqeuent word already exists
+                if next in value_dict:
+                    # Then just update the running count
+                    word_count = value_dict[next]
+                    word_count.count += 1
+                else:
+                    # Create a new word count
+                    word_count = WordCount(next, 1)
+                    value_dict[next] = word_count
+            else:
+                # Make a new word count.
+                word_count = WordCount(next, 1)
+                
+                # Since key didn't exist before, we need 
+                # to create the value dict. 
+                value_dict = {}
+                value_dict[curr] = word_count
+                result[curr] = value_dict
+        return result
